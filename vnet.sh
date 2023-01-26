ddf6f86a19dc8b84cda66c9b0bc8a4b1c1199aea
NetBSD User <netbsduser@users.noreply.github.com>
2023-01-26 10:50:26 +0000

    general: add a script for testing net

---
+++ vnet.sh
@@ -0,0 +1,5 @@
+qemu-system-x86_64 -cdrom build/barebones.iso  -s  -serial stdio \
+  -drive file=hda.img,if=virtio \
+  -netdev tap,id=mynet0,ifname=tap0,script=no,downscript=no \
+  -device virtio-net-pci,netdev=mynet0 \
+  -smp 1 -enable-kvm
