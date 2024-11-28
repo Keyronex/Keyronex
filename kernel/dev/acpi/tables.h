6c732f27baa2b14d4d8ad1eff513ebbbf7a131d5
NetBSD User <netbsduser@users.noreply.github.com>
2024-11-28 22:15:51 +0000

    ACPI: Don't drop GTDT table definition.

--- kernel/dev/acpi/tables.h
+++ kernel/dev/acpi/tables.h
@@ -16,4 +16,26 @@ typedef struct acpi_header_t {
 	uint32_t creator_revision;
 } __attribute__((packed)) acpi_header_t;
 
+typedef struct acpi_table_gtdt {
+	acpi_header_t header;
+	uint64_t counter_block_address;
+	uint32_t reserved;
+	uint32_t secure_el1_interrupt;
+	uint32_t secure_el1_flags;
+	uint32_t nonsecure_el1_interrupt;
+	uint32_t nonsecure_el1_flags;
+	uint32_t virtual_timer_interrupt;
+	uint32_t virtual_timer_flags;
+	uint32_t nonsecure_el2_interrupt;
+	uint32_t nonsecure_el2_flags;
+	uint64_t counter_read_block_address;
+	uint32_t platform_timer_count;
+	uint32_t platform_timer_offset;
+} __attribute__((packed)) acpi_table_gtdt_t;
+
+/* Is it edge triggered? */
+#define ACPI_GTDT_INTERRUPT_MODE (1)
+/*! Is it active low? */
+#define ACPI_GTDT_INTERRUPT_POLARITY (1 << 1)
+
 #endif /* KRX_ACPI_TABLES_H */
