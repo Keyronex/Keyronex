fc1af0870d2987b58b9eb3e2f5ae8a04184724d8
NetBSD User <netbsduser@users.noreply.github.com>
2026-03-05 22:34:45 +0000

    Linux affinity: Basic NetLink headers.

---
+++ kernel/common/linux/netlink.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2026 Cloudarox Solutions.
+ * Created on Thu Mar 05 2026.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+/*!
+ * @file netlink.h
+ * @brief NetLink
+ */
+
+#ifndef ECX_LINUX_NETLINK_H
+#define ECX_LINUX_NETLINK_H
+
+#include <sys/socket.h>
+
+#include <stdint.h>
+
+#define NETLINK_ROUTE 0
+
+struct sockaddr_nl {
+	uint8_t		nl_len;
+	sa_family_t	nl_family;
+	uint16_t	nl_pad;
+	uint32_t	nl_pid;
+	uint32_t	nl_groups;
+};
+
+struct nlmsghdr {
+	uint32_t	nlmsg_len;
+	uint16_t	nlmsg_type;
+	uint16_t	nlmsg_flags;
+	uint32_t	nlmsg_seq;
+	uint32_t	nlmsg_pid;
+};
+
+#define NLM_F_REQUEST		0x1
+#define NLM_F_ACK		0x4
+#define NLM_F_EXCL		0x200
+#define NLM_F_CREATE		0x400
+
+#define NLMSG_NOOP	0x1
+#define NLMSG_ERROR	0x2
+
+struct nlmsgerr {
+	int	error;
+	struct	nlmsghdr msg;
+};
+
+#define NLMSG_ALIGNTO		4
+#define NLMSG_ALIGN(len) \
+    (((len) + NLMSG_ALIGNTO - 1) & ~(NLMSG_ALIGNTO - 1))
+#define NLMSG_HDRLEN \
+    NLMSG_ALIGN(sizeof(struct nlmsghdr))
+#define NLMSG_LENGTH(len)	((len) + NLMSG_HDRLEN)
+#define NLMSG_DATA(nlh)	 \
+    ((void *)((char *)(nlh) + NLMSG_HDRLEN))
+#define NLMSG_OK(nlh, remaining) \
+    ((len) >= (int)sizeof(struct nlmsghdr) && \
+	(nlh)->nlmsg_len >= sizeof(struct nlmsghdr) && \
+	(nlh)->nlmsg_len <= (len))
+#define NLMSG_NEXT(nlh, len) \
+    ((len) -= NLMSG_ALIGN((nlh)->nlmsg_len), \
+    	(struct nlmsghdr *)((char *)(nlh) + NLMSG_ALIGN((nlh)->nlmsg_len)))
+
+#endif /* ECX_LINUX_NETLINK_H */
---
+++ kernel/common/linux/rtnetlink.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2026 Cloudarox Solutions.
+ * Created on Thu Mar 05 2026.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+/*!
+ * @file rtnetlink.h
+ * @brief NetLink for routing
+ */
+
+#ifndef ECX_LINUX_RTNETLINK_H
+#define ECX_LINUX_RTNETLINK_H
+
+#include <stdint.h>
+
+#define RTM_NEWROUTE		24
+
+#define RT_TABLE_MAIN		254
+
+#define RTPROT_BOOT		3
+#define RTPROT_STATIC		4
+
+#define RT_SCOPE_UNIVERSE	0
+
+#define RTN_UNICAST		1
+
+#define RTA_OIF			4
+#define RTA_GATEWAY		5
+
+struct rtmsg {
+	unsigned char	rtm_family;
+	unsigned char	rtm_dst_len;
+	unsigned char	rtm_src_len;
+	unsigned char	rtm_tos;
+	unsigned char	rtm_table;
+	unsigned char	rtm_protocol;
+	unsigned char	rtm_scope;
+	unsigned char	rtm_type;
+	unsigned int	rtm_flags;
+};
+
+struct rtattr {
+	uint16_t	rta_len;
+	uint16_t	rta_type;
+};
+
+#define RTA_ALIGNTO	4
+#define RTA_ALIGN(len)	(((len) + RTA_ALIGNTO - 1) & ~(RTA_ALIGNTO - 1))
+#define RTA_OK(rta, remaining)	\
+    ((remaining) >= (int)sizeof(struct rtattr) &&  \
+	(rta)->rta_len >= sizeof(struct rtattr) && \
+	(rta)->rta_len <= (remaining))
+#define RTA_LENGTH(len)	(RTA_ALIGN(sizeof(struct rtattr)) + (len))
+#define RTA_DATA(rta)	\
+    ((void *)((char *)(rta) + RTA_ALIGN(sizeof(struct rtattr))))
+
+#endif /* ECX_LINUX_RTNETLINK_H */
